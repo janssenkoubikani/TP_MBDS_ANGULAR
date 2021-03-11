cf68f4ccbcc9c7f25bc76d33e3964433214de847
janssen <32483059+janssenkoubikani@users.noreply.github.com>
2021-03-11 19:36:18 +0100

    first commit

---
+++ api/app.js
@@ -0,0 +1,40 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
+
+const Thing = require('./models/Thing');
+
+const app = express();
+
+mongoose.connect('mongodb+srv://peace:peace@cluster0.7goae.mongodb.net/thing?retryWrites=true&w=majority',
+  { useNewUrlParser: true,
+    useUnifiedTopology: true })
+  .then(() => console.log('Connexion à MongoDB réussie !'))
+  .catch(() => console.log('Connexion à MongoDB échouée !'));
+
+app.use((req, res, next) => {
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization');
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
+  next();
+});
+
+app.use(bodyParser.json());
+
+app.post('/api/stuff', (req, res, next) => {
+  delete req.body._id;
+  const thing = new Thing({
+    ...req.body
+  });
+  thing.save()
+    .then(() => res.status(201).json({ message: 'Assignemnt enregistré !'}))
+    .catch(error => res.status(400).json({ error }));
+});
+
+app.use('/api/stuff', (req, res, next) => {
+  Thing.find()
+    .then(things => res.status(200).json(things))
+    .catch(error => res.status(400).json({ error }));
+});
+
+module.exports = app;
